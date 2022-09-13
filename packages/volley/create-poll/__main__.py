339659249e9f55fbcc9c3b102ba8d5dcb7ad72bf
Rui <ruicunham@hotmail.com>
2022-09-13 16:07:15 +0100

    First commit

---
+++ packages/volley/create-poll/__main__.py
@@ -0,0 +1,38 @@
+from payloads import MATTER_PAYLOAD
+from datetime import date, timedelta
+import scrapper
+import rally
+
+TOKEN = "***REMOVED***"
+CHANNEL = "bottest"
+
+
+def main(args):
+    request_token = args.get("token", "TOKEN")
+    request_channel = args.get("channel_name", "ASDASD")
+
+    # if request_token != TOKEN or request_channel != CHANNEL:
+    #     return {"statusCode": 403}
+
+    today = date.today()
+    next_monday = today + timedelta(days=-today.weekday(), weeks=1)
+
+    try:
+        slots = scrapper.get_slots(next_monday)
+        rally_url = rally.create_poll(slots, next_monday)
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "body": str(e),
+        }
+
+    MATTER_PAYLOAD["attachments"][0]["fallback"] = rally_url
+    MATTER_PAYLOAD["attachments"][0]["text"] = rally_url
+    MATTER_PAYLOAD["attachments"][0]["title"] = f"Volleyball week {next_monday}"
+
+    return {
+        "headers": {
+            "content-type": "application/json",
+        },
+        "body": MATTER_PAYLOAD,
+    }
