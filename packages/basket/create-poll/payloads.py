30071a8769f618931a1c3be7861352c4814ce9f6
Rui <ruicunham@hotmail.com>
2022-11-27 16:04:38 +0000

    Add basketball package

---
+++ packages/basket/create-poll/payloads.py
@@ -0,0 +1,29 @@
+# Payloads
+MATTER_PAYLOAD = {
+    "username": "Basketball",
+    "icon_url": "https://media.istockphoto.com/id/876387766/photo/basketball-black-background-isolated.jpg?s=170667a&w=0&k=20&c=dpdMB2we8AmAE5kaI_dTBmyMRi1TmBIf3C6UogDSvg0=",
+    "response_type": "in_channel",
+    "attachments": [
+        {
+            "color": "#FFA500",
+            "author_name": "Basketball",
+            "author_icon": "https://media.istockphoto.com/id/876387766/photo/basketball-black-background-isolated.jpg?s=170667a&w=0&k=20&c=dpdMB2we8AmAE5kaI_dTBmyMRi1TmBIf3C6UogDSvg0=",
+        }
+    ],
+}
+
+RALLY_PAYLOAD = {
+    "0": {
+        "json": {
+            "title": "Basketball",
+            "type": "date",
+            "location": "Nave 2",
+            "description": "",
+            "user": {
+                "name": "Basketball BOT",
+                "email": "rpmonteiro25@hotmail.com",
+            },
+            "timeZone": "Europe/Lisbon",
+        }
+    }
+}
---
+++ packages/basket/create-poll/rally.py
@@ -0,0 +1,31 @@
+from payloads import RALLY_PAYLOAD
+from requests import post, get
+from string import Template
+
+RALLY_CREATE_URL = "https://rallly.co/api/trpc/polls.create?batch=1"
+RALLY_GET_URL = Template(
+    'https://rallly.co/api/trpc/polls.get?batch=1&input={"0":{"json":{"urlId":"$urlId","admin":true}}}'
+)
+
+
+def create_poll(slots, date):
+    RALLY_PAYLOAD["0"]["json"]["options"] = slots
+    RALLY_PAYLOAD["0"]["json"]["description"] = f"Basketball week {date}"
+
+    response = post(RALLY_CREATE_URL, json=RALLY_PAYLOAD)
+
+    if not response.ok:
+        raise Exception("Error creating Rally poll")
+
+    urlId = response.json()[0]["result"]["data"]["json"]["urlId"]
+
+    response = get(RALLY_GET_URL.substitute(urlId=urlId))
+
+    if not response.ok:
+        raise Exception("Error obtaining participantUrlId")
+
+    participantUrlId = response.json()[0]["result"]["data"]["json"][
+        "participantUrlId"
+    ]
+
+    return f"https://rallly.co/p/{participantUrlId}"
