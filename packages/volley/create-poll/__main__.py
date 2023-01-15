b726847de592b9a002546c3fa76c13efccb2f8aa
Rui <rpmonteiro24@hotmail.com>
2023-01-15 11:10:40 +0000

    [Volley] Allow creating poll for current week

--- packages/volley/create-poll/__main__.py
+++ packages/volley/create-poll/__main__.py
@@ -14,16 +14,28 @@ VALID_CHANNELS = [
 def main(args):
     request_token = args.get("token", "TOKEN")
     request_channel = args.get("channel_name", "CHANNEL")
+    request_text = args.get("text", "")
 
     if request_token != TOKEN or request_channel not in VALID_CHANNELS:
         return {"statusCode": 403}
 
     today = date.today()
-    next_monday = today + timedelta(days=-today.weekday(), weeks=1)
+
+    if not request_text:
+        start = today + timedelta(days=-today.weekday(), weeks=1)
+        title = "Rallly for next week!" 
+    elif request_text == "current":
+        start = today + timedelta(days=1)
+        title = "Rallly for this week!"
+    else:
+        return {
+            "statusCode": 400,
+            "body": "Invalid argument",
+        }
 
     try:
-        slots = scrapper.get_slots(next_monday)
-        rally_url = rally.create_poll(slots, next_monday)
+        slots = scrapper.get_slots(start)
+        rally_url = rally.create_poll(slots, start)
     except Exception as e:
         return {
             "statusCode": 400,
@@ -33,12 +45,12 @@ def main(args):
     MATTER_PAYLOAD["text"] = "<!channel>"
     MATTER_PAYLOAD["attachments"][0]["fallback"] = rally_url
     MATTER_PAYLOAD["attachments"][0]["text"] = rally_url
-    MATTER_PAYLOAD["attachments"][0]["title"] = f"Rallly for next week!"
+    MATTER_PAYLOAD["attachments"][0]["title"] = title 
     MATTER_PAYLOAD["attachments"][0]["fields"] = [
         {
             "short": True,
             "title": "Week",
-            "value": f"{next_monday}",
+            "value": f"{start}",
         },
     ]
 
