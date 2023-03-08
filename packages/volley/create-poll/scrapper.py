b115f2cff4746e7db55cdc0958fa6407ce0e7adb
Rui <rpmonteiro24@hotmail.com>
2023-03-08 18:01:33 +0000

    Remove Wednesday check

--- packages/volley/create-poll/scrapper.py
+++ packages/volley/create-poll/scrapper.py
@@ -16,29 +16,26 @@ def get_slots(start_date):
     slots = []
     for i in range(5 - start_date.weekday()):
         curr_date = start_date + timedelta(days=i)
-        
-        if curr_date.weekday() != 2:  # Ignore Wednesday
-            iso_date = curr_date.isoformat()
-            response = get(UM_TEMPLATE_URL.substitute(date=iso_date))
 
-            if not response.ok:
-                raise Exception("Error retrieving UM data")
+        iso_date = curr_date.isoformat()
+        response = get(UM_TEMPLATE_URL.substitute(date=iso_date))
 
-            soup = BeautifulSoup(response.content, "html.parser")
+        if not response.ok:
+            raise Exception("Error retrieving UM data")
 
-            results = soup.find_all("div", class_="col-sm-7")[1]
-            available = results.find_all(
-                "button", class_="btn btn-primary btn-m btn_reserva"
-            )
-            available = [x.text for x in available]
-            for h1, h2 in VALID_HOURS:
-                if h1 in available and h2 in available:
-                    start_time = time.fromisoformat(h1)
-                    end_time = time(
-                        start_time.hour + 1, start_time.minute, start_time.second
-                    )
-                    slots.append(
-                        f"{iso_date}T{start_time}/{iso_date}T{end_time}"
-                    )
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        results = soup.find_all("div", class_="col-sm-7")[1]
+        available = results.find_all(
+            "button", class_="btn btn-primary btn-m btn_reserva"
+        )
+        available = [x.text for x in available]
+        for h1, h2 in VALID_HOURS:
+            if h1 in available and h2 in available:
+                start_time = time.fromisoformat(h1)
+                end_time = time(
+                    start_time.hour + 1, start_time.minute, start_time.second
+                )
+                slots.append(f"{iso_date}T{start_time}/{iso_date}T{end_time}")
 
     return slots
