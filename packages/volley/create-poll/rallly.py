6425ab91b0f17504e7f91276f2921fec8dd16018
Rui <ruicunham@hotmail.com>
2024-03-11 21:32:58 +0000

    Fix Rallly poll creation
    Use a Session object to persist cookies

--- packages/volley/create-poll/rallly.py
+++ packages/volley/create-poll/rallly.py
@@ -1,11 +1,16 @@
 from payloads import rallly_payload
-from requests import post
+import requests
 
+RALLLY_URL = "https://app.rallly.co/new"
 RALLLY_CREATE_URL = "https://app.rallly.co/api/trpc/polls.create?batch=1"
 
 
 def create_poll(slots, date):
-    response = post(RALLLY_CREATE_URL, json=rallly_payload(date, slots))
+    session = requests.Session()
+    session.get(RALLLY_URL)
+    response = session.post(
+        RALLLY_CREATE_URL, json=rallly_payload(date, slots)
+    )
 
     if not response.ok:
         raise Exception(f"Error creating Rallly poll: {response.json()}")
