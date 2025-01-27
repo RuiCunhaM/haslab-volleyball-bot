8a48c3bebe8071c93fa40e827753fb7683596289
rui <ruicunham@hotmail.com>
2025-01-27 15:55:45 +0000

    fix(rally): handle CSRF token issues

--- packages/volley/create-poll/rallly.py
+++ packages/volley/create-poll/rallly.py
@@ -1,13 +1,25 @@
 from payloads import rallly_payload
 import requests
 
-RALLLY_URL = "https://app.rallly.co/new"
+RALLLY_URL_1 = "https://app.rallly.co/api/auth/csrf"
+RALLLY_URL_2 = "https://app.rallly.co/api/auth/callback/guest"
+RALLLY_URL_3 = "https://app.rallly.co/api/auth/session"
 RALLLY_CREATE_URL = "https://app.rallly.co/api/trpc/polls.create?batch=1"
 
 
 def create_poll(slots, date):
     session = requests.Session()
-    session.get(RALLLY_URL)
+    token = session.get(RALLLY_URL_1).json()["csrfToken"]
+    session.post(
+        RALLLY_URL_2,
+        data={
+            "redirect": "false",
+            "csrfToken": token,
+            "callbackUrl": "https://app.rallly.co/new",
+            "json": "true",
+        },
+    )
+    session.get(RALLLY_URL_3)
     response = session.post(RALLLY_CREATE_URL, json=rallly_payload(date, slots))
 
     if not response.ok:
